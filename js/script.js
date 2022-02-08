4dfb9fa11c82eea596f362c8514cb8fb54bb0048
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-09 02:44:04 +0600

    delete added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,41 @@
 // console.log('welcome')
 let count = 0;
+const tableBody = document.getElementById('table-body');
+
 document.getElementById('add-button').addEventListener('click',function(){
-    const inputField = document.getElementById('add-field').value;
+    const inputField = document.getElementById('add-field');
     console.log()
     // console.log(inputField);
-    if(inputField != '' ){
+    if(inputField.value != '' ){
         count++;
         // console.log(inputField);
-        const tableBody = document.getElementById('table-body');
+        
 
         // console.log(tableBody);
 
         const tableRow = document.createElement('tr');
         tableRow.innerHTML = `<th scope="row">${count}</th>
-        <td>${inputField}</td>
-        <td>Otto</td>        
+        <td>${inputField.value}</td>
+        <td>
+        <button class="btn btn-danger" id="delete-button">Delete</button>
+        <button class="btn btn-success ms-2">Done</button>
+        </td>        
         `;
         tableBody.appendChild(tableRow);
+        inputField.value = '';
 
     }else{
         alert('Empty Field No Data Found')
+    }
+})
+
+tableBody.addEventListener('click',function(e){
+    // console.log(e.target.innerText);
+    if(e.target.innerText == "Delete"){
+        count--;
+         const parentNode = e.target.parentNode.parentNode.parentNode;
+        const childNode = e.target.parentNode.parentNode;
+        parentNode.removeChild(childNode); 
+        console.log(e.target.parentNode.parentNode.parentNode);
     }
 })
